812111b5b68fbcdb5056417f83eeeffe6790cbcd
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-04-14 16:28:27 +0800

    Another type of user was added. (guest user)

---
+++ src/ph/edu/dlsu/CameraGuest.java
@@ -0,0 +1,241 @@
+package ph.edu.dlsu;
+
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoWriter;
+import ph.edu.dlsu.utils.ScreenSize;
+import ph.edu.dlsu.utils.Sound;
+import ph.edu.dlsu.utils.Utils;
+
+import java.io.*;
+
+public class CameraGuest extends BaseCameraScene{
+
+    private boolean takePicture = false;
+
+    private VideoWriter videoWriter;
+    private int frames;
+
+    int imageCount = imageCount();
+    int videoCount = videoCount();
+
+    public int videoCount(){
+        String fileName = "Shots/VidClips/videoCount.txt";
+
+        String line = null;
+
+        int videoCount = 1;
+
+        BufferedReader bufferedReader = null;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+
+            bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                videoCount = Integer.parseInt(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open file!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null){
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+        return videoCount;
+    }
+
+    public int imageCount(){
+        String fileName = "Shots/Snaps/imageCount.txt";
+
+        String line = null;
+
+        int imageCount = 1;
+
+        BufferedReader bufferedReader = null;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+
+            bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                imageCount = Integer.parseInt(line);
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file!");
+        } catch (IOException e) {
+            System.out.println("Error reading file!");
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+
+            }
+        }
+        return imageCount;
+    }
+
+    @Override
+    public Parent createCameraContent(){
+        try {
+            initializeCapture();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ScreenSize screen = new ScreenSize();
+        displayWidth = screen.getDisplayWidth();
+        displayHeight = screen.getDisplayHeight();
+
+        frameWidth = 0.8 * displayWidth;
+        frameHeight = displayHeight;
+
+        Pane rootNode = new Pane();
+        rootNode.setPrefSize(displayWidth, displayHeight);
+
+        ImageView imgBackground = Utils.loadImage2View("res//images//Green-Screen-Center.png", displayWidth, displayHeight);
+        if(imgBackground != null){
+            rootNode.getChildren().add(imgBackground);
+        }
+
+        currentFrame = Utils.loadImage2View("res//images//Green-Screen-Center.png", frameWidth, frameHeight);
+        currentFrame.setTranslateX((displayWidth - frameWidth)/2.0);
+        currentFrame.setTranslateY(0);
+        rootNode.getChildren().add(currentFrame);
+        startCamera();
+        createHMenu();
+
+        rootNode.getChildren().add(menuBox);
+
+        return rootNode;
+    }
+
+    private void initializeCapture() throws IOException{
+        frameHeight = 0;
+        frameWidth = 0;
+        int fps = 20; //Lower value means slow motion; Higher value means fast motion; value is equal to minute in real time
+
+        String outputFile = "Shots/VidClips/vidClip" + videoCount + ".avi";
+        int fourCC = VideoWriter.fourcc('i', 'y', 'u', 'v');
+
+        videoWriter = new VideoWriter(outputFile, fourCC, fps, new Size(frameWidth, frameHeight), true);
+
+        if (!videoWriter.isOpened()){
+            System.out.println("Unable to record a video!");
+        }
+
+        System.out.println("videoCount: " + videoCount);
+
+        frames = 0;
+        videoCount++;
+
+        String videoCount = Integer.toString(this.videoCount);
+        File file = new File("Shots/VidClips/videoCount.txt");
+        BufferedWriter out = new BufferedWriter(new FileWriter(file));
+        out.write(videoCount);
+        out.close();
+    }
+
+    @Override
+    public void createHMenu() {
+        final CustomMenuItem home = new CustomMenuItem("home", menuWidth, menuHeight);
+        final CustomMenuItem capture = new CustomMenuItem("capture", menuWidth, menuHeight);
+        final CustomMenuItem logout = new CustomMenuItem("logout", menuWidth, menuHeight);
+        final CustomMenuItem exit = new CustomMenuItem("exit", menuWidth, menuHeight);
+
+        home.setOnMouseClicked(e -> {
+            stopCamera();
+            Main.onHome();
+        });
+
+        capture.setOnMouseClicked(event -> {
+
+            (new Sound("file:res/sounds/camera.mp3")).play();
+            takePicture = true;
+
+        });
+
+        logout.setOnMouseClicked(e -> {
+            stopCamera();
+            Main.onLogIn();
+        });
+
+        exit.setOnMouseClicked(e -> {
+            Boolean confirmQuit = Main.onExit();
+            if(confirmQuit){
+                stopCamera();
+            }
+        });
+
+        menuBox = new MenuHBox(home, capture, logout, exit);
+
+        menuBox.setTranslateX((displayWidth - 4 * menuWidth)/2.0);
+        menuBox.setTranslateY(0);
+
+    }
+
+    @Override
+    public void onCameraFrame(Mat frame) throws IOException {
+
+        if (frames >= 0 && frames < 1200) { //frame 0 to frame n, where n is equal to fps declared at initCapture * 60 * desired duration in minutes
+            System.out.println(frames);
+            videoWriter.write(frame);
+            frames++;
+        }else{
+            videoWriter.release();
+            if(videoCount > 10){
+                videoCount = 1;
+            }
+            initializeCapture();
+        }
+
+        if (takePicture){
+            String fileName = "snap" + imageCount + ".png";
+
+            Imgcodecs.imwrite("Shots/Snaps/" + fileName, frame);
+
+            takePicture = false;
+
+            if(imageCount <= 20) {
+                if (imageCount == 20)
+                    imageCount = 1;
+                else
+                    imageCount++;
+            }
+            else
+                imageCount = 1;
+
+            String imageCount = Integer.toString(this.imageCount);
+            File file = new File("Shots/Snaps/imageCount.txt");
+            BufferedWriter out = new BufferedWriter(new FileWriter(file));
+            out.write(imageCount);
+            out.close();
+
+        }
+
+    }
+
+    @Override
+    public void stopCamera(){
+        if (videoWriter != null)
+            videoWriter.release();
+        super.stopCamera();
+    }
+
+}
---
+++ src/ph/edu/dlsu/HomeGuest.java
@@ -0,0 +1,77 @@
+package ph.edu.dlsu;
+
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import ph.edu.dlsu.utils.ScreenSize;
+import ph.edu.dlsu.utils.Utils;
+
+public class HomeGuest {
+
+    MenuVBox menuBox;
+    ScreenSize screen = new ScreenSize();
+    double displayWidth = screen.getDisplayWidth();
+    double displayHeight = screen.getDisplayHeight();
+
+    public Parent main(){
+
+        Pane rootNode = new Pane();
+        rootNode.setPrefSize(displayWidth, displayHeight);
+
+        ImageView imgBackground = Utils.loadImage2View("res//images//Green-Screen-Right.png", displayWidth, displayHeight);
+        if (imgBackground != null) {
+            rootNode.getChildren().add(imgBackground);
+        }
+
+        MenuTitle title = new MenuTitle("menu");
+        title.setTranslateX(200);
+        title.setTranslateY(200);
+
+        createVMenu();
+
+        rootNode.getChildren().addAll(title, menuBox);
+
+        return rootNode;
+    }
+
+    public void createVMenu() {
+
+        final CustomMenuItem camera = new CustomMenuItem("camera");
+        final CustomMenuItem videoclips = new CustomMenuItem("video clips");
+        final CustomMenuItem snapshots = new CustomMenuItem("snapshots");
+        final CustomMenuItem logout = new CustomMenuItem("logout");
+        final CustomMenuItem setting = new CustomMenuItem("setting");
+        final CustomMenuItem exit = new CustomMenuItem("exit");
+
+
+        camera.setOnMouseClicked(e -> {
+            Main.onCamera();
+        });
+
+        videoclips.setOnMouseClicked(e -> {
+            Main.onVideoClips();
+        });
+
+        snapshots.setOnMouseClicked(e -> {
+            Main.onSnapShots();
+        });
+
+        logout.setOnMouseClicked(e -> {
+            Main.onLogIn();
+        });
+
+        setting.setOnMouseClicked(e -> {
+            Main.onSetting();
+        });
+
+        exit.setOnMouseClicked(e -> {
+            Main.onExit();
+        });
+
+        menuBox = new MenuVBox(camera, videoclips, snapshots, logout, setting, exit);
+        menuBox.setTranslateX(200);
+        menuBox.setTranslateY(300);
+
+
+    }
+}
